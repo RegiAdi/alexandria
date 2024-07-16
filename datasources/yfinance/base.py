84f0cc36b899aec55adf164d28b0513f6b05d727
Regi Adi <regiadi.priyanto@gmail.com>
2024-07-17 02:44:51 +0700

    Add mongodb connection

--- datasources/yfinance/base.py
+++ datasources/yfinance/base.py
@@ -1,5 +1,18 @@
 import sys
 import yfinance as yf
+from pymongo import MongoClient
+
+
+def get_database():
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = "mongodb://localhost:27017/"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client["alexandria"]
 
 
 def get_ticker(ticker_symbol):
@@ -44,8 +57,8 @@ ticker_symbol = sys.argv[1] + ".JK"
 print(ticker_symbol)
 
 ticker = get_ticker(ticker_symbol)
-export_to_csv_income_statement(ticker)
-export_to_csv_balance_sheet(ticker)
+# export_to_csv_income_statement(ticker)
+# export_to_csv_balance_sheet(ticker)
 
 income_statement = get_income_statement(ticker)
 print(income_statement.index)
@@ -55,6 +68,14 @@ print(income_statement.T)
 share_count = ticker.get_shares_full(start="2024-01-01", end=None)
 
 df_json = income_statement.to_json()
+# df_dict = income_statement.to_dict()
 
 print(df_json)
-print(share_count)
+# print(df_dict)
+# print(share_count)
+
+income_statement_data = {"income_statement": df_json}
+
+db = get_database()
+income_statement_collection = db["income_statement"]
+income_statement_collection.insert_one(income_statement_data)
