d5dcddeac572767050bb96a6a097c4a45da10564
Regi Adi <regiadi.priyanto@gmail.com>
2025-01-10 14:13:07 +0700

    Remove currency from tickers

--- tickers.py
+++ tickers.py
@@ -1,25 +1,21 @@
 tickers = {
     "ADRO.JK": {
         "balance_sheet": {
-            "currency": "USD",
             "freq": ['yearly', 'quarterly']
         }
     },
     "BSSR.JK": {
         "balance_sheet": {
-            "currency": "IDR",
             "freq": ['yearly', 'quarterly']
         }
     },
     "ITMG.JK": {
         "balance_sheet": {
-            "currency": "USD",
             "freq": ['yearly', 'quarterly']
         }
     },
     "PTBA.JK": {
         "balance_sheet": {
-            "currency": "IDR",
             "freq": ['yearly', 'quarterly']
         }
     },
